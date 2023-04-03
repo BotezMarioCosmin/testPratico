39348d24b1077ad9e3ee71ab162b471c1fc32ea3
botez.19313 <botez.mariocosmin.studente@itispaleocapa.it>
2023-04-03 08:58:27 +0200

    task 1

---
+++ testPratico/Cellulare.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPratico
+{
+    public class Cellulare : Dispositivo
+    {
+        private float _ram;
+        private float _capienza;
+
+        public float Ram
+        {
+            set
+            {
+                if (value > 0.00 && value < 32.01)
+                {
+                    _ram = value;
+                }
+                else
+                {
+                    throw new Exception("Dimensione ram non valida!");
+                }
+            }
+            get { return _ram; }
+        }
+
+        public float Capienza
+        {
+            set
+            {
+                if (value > 0.00 && value < 1000.01)
+                {
+                    _capienza = value;
+                }
+                else
+                {
+                    throw new Exception("Dimensione archiviazione non valida!");
+                }
+            }
+            get { return _capienza; }
+        }
+
+
+        public Cellulare(int numser, string modello, float ram, float capienza) : base(numser, modello)
+        {
+            Ram = ram;
+            Capienza = capienza;
+        }
+
+        public Cellulare()
+        {
+            Ram = -1;
+            Capienza = -1;
+        }
+
+        public override float CalcolaPrezzo()//30 euro per giga ram, 32gb aggiungi 50 euro
+        {
+            float prezzo = 0;
+            prezzo = Ram * 30;
+
+            float tmp = 0;
+            tmp = Capienza / 32;
+            tmp = tmp * 50;
+
+            return prezzo + tmp;
+        }
+    }
+}
---
+++ testPratico/Dispositivo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPratico
+{
+    public abstract class Dispositivo
+    {
+        private int _numser;
+        private string _modello;
+
+        public int Numser
+        {
+            get { return _numser; }
+            set { _numser = value; }
+        }
+
+        public string Modello
+        {
+            get { return _modello; }
+            set { _modello = value; }
+        }
+
+        public Dispositivo(int numser, string modello)
+        {
+            Numser = numser;
+            Modello = modello;
+        }
+
+        public Dispositivo()
+        {
+            Numser = -1;
+            Modello = "NullModello";
+        }
+
+        public Dispositivo(int numser)
+        {
+            Numser = numser;
+            Modello = "NullModello";
+        }
+
+        public Dispositivo(string modello)
+        {
+            Modello = modello;
+            Numser = -1;
+        }
+
+        public abstract float CalcolaPrezzo();
+
+    }
+}
